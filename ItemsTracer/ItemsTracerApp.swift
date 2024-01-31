f23e0f6161d5687df6ec5ea747a95c8d4846d759
Luc Nguyen <luc@Lucs-MBP.lan>
2024-01-31 04:43:10 +0100

    feat: init repo

---
+++ ItemsTracer/ItemsTracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemsTracerApp.swift
+//  ItemsTracer
+//
+//  Created by Luc Nguyen on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ItemsTracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
