53cef3821d93a98cdcaf59bdfeb8913051973329
Luc Nguyen <luc@Lucs-MBP.lan>
2024-03-11 21:55:02 +0100

    feat: Inventory List View Done

--- ItemsTracer/Views/InventoryListView.swift
+++ ItemsTracer/Views/InventoryListView.swift
@@ -13,6 +13,11 @@ struct InventoryListView: View {
         List {
             ForEach(vm.items){
                 item in InventoryListItemView(item: item)
+                    .listRowSeparator(.hidden)
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        
+                    }
             }
         }
         .navigationTitle("Item Tracer")
@@ -25,10 +30,29 @@ struct InventoryListItemView: View {
     let item: InventoryItem
     var body: some View {
         HStack(alignment: .top, spacing: 16){
+            
+            ZStack{
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(Color.gray.opacity(0.3))
+                if let thumbnailURL = item.thumbnailURL{
+                    AsyncImage(url: thumbnailURL){ phase in
+                        switch phase {
+                        case .success(let image):
+                            image.resizable()
+                                .aspectRatio(contentMode: .fit)
+                        default:
+                            ProgressView()
+                        }
+                    }
+                }
+            }
+            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray.opacity(0.3), lineWidth:1))
+            .frame(width: 150, height: 150)
             VStack(alignment: .leading){
                 Text(item.name)
                     .font(.headline)
                 Text("Quantity: \(item.quantity)")
+                    .font(.subheadline)
             }
         }
     }
@@ -36,6 +60,6 @@ struct InventoryListItemView: View {
 #Preview {
     NavigationStack{
         InventoryListView()
-
+        
     }
 }
